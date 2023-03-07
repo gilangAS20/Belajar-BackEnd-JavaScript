10b2b04987c2fdd73fbc1c0de8267289b7aee04a
Gilang Agung Saputra <84905621+gilangAS20@users.noreply.github.com>
2023-03-07 16:58:00 +0700

    Add files via upload

---
+++ belajar5_if_else_statement.js
@@ -0,0 +1,53 @@
+// ================If Else Statement================
+console.log("================If Else Statement================ \n\n");
+// If-Else Statement berfungsi untuk mengambil sebuah keputusan berdasarkan kondisi tertentu
+
+// Contoh 1
+console.log("Contoh 1 : \n");
+
+let isRain = true;
+console.log("Persiapan sebelum berangkat aktivitas \n");
+console.log("Apakah hujan? ", isRain);
+
+if (isRain) {
+    console.log("Bawa payung atau jas hujan");
+}
+else {
+    console.log("Tidak perlu bawa payung, tapi untuk jaga-jaga oke saja")
+}
+console.log("Hati-hati di jalan ya \n\n");
+
+// Contoh 2
+console.log("Contoh 2 : \n");
+let nilai = 65;
+console.log("Nilai Anda adalah ", nilai);
+
+if (nilai >= 70){
+    console.log("Selamat!, Anda lulus dengan nilai "+ nilai +", aman kok");
+}
+else if (nilai >= 65) {
+    console.log("Anda lulus dengan nilai "+ nilai +", namun baiknya ikut remidi");
+}
+else{
+    console.log("Maaf Anda tidak lulus, nilai Anda "+ nilai +", jangan lupa ikut remidi ya");
+}
+
+console.log("\n");
+
+// Contoh 3
+console.log("Contoh 3 : \n");
+
+let language = "Indonesia";
+let greeting = "Selamat Pagi"
+
+if(language === "English") {
+    greeting = "Good Morning!";
+} else if(language === "French") {
+    greeting = "Bonjour!"
+} else if(language === "Japanese") {
+    greeting = "Ohayou Gozaimasu!"
+} else if(language == "Indonesia") {
+  	greeting
+}
+
+console.log(greeting);
