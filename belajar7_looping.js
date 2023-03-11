55a2f511726e77c499e665c298281a0352140c26
Gilang Agung Saputra <84905621+gilangAS20@users.noreply.github.com>
2023-03-11 22:00:15 +0700

    Baru sampai for loop

---
+++ belajar7_looping.js
@@ -0,0 +1,23 @@
+//===========Looping================
+console.log("================Looping================ \n\n");
+
+
+// Contoh Looping dengan "For"
+console.log("Contoh Looping dengan 'For'");
+let nilai = 0;
+for (nilai; nilai < 10; nilai++) 
+{
+    console.log(nilai);
+}
+
+console.log("\n")
+// Contoh Looping dengan "For of"
+console.log("Contoh Looping dengan 'For of'");
+let myArray = ["gilang", "agung", "saputra", "anjay", "meledak", "bummmm"];
+for (const arrayItem of myArray) 
+{
+    console.log(arrayItem);
+}
+
+console.log("\n");
+// Contoh Looping dengan "While" dan "Do While"
